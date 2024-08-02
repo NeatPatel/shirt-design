471764f269f2659bbdbf9e72569d9623391a9e04
FishReddy <vishwareddym@gmail.com>
2024-08-01 21:38:30 -0700

    added navbar and footer

---
+++ App/src/components/Nbar.jsx
@@ -0,0 +1,30 @@
+import styles from './navbar.module.scss';
+import { Link, Outlet } from 'react-router-dom';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import Footer from './Footer.jsx';
+
+function Nbar() {
+    return (<>
+        <Navbar variant='dark' className="navbar-primary bg-primary border-primary" expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/" className="nav-link">Soramo Design Playground</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="mainNavPanel" />
+                <Navbar.Collapse className="text-end" id="mainNavPanel">
+                    <Nav className="ms-auto align-items-center" navbarScroll>
+                        <Link to="/about" className="nav-link">About</Link>
+                        <Link to="/pricing" className="nav-link">Pricing</Link>
+                        <Link to="/contact-us" className="nav-link">Editor</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        <Outlet />
+
+        <Footer />
+    </>);
+}
+
+export { Nbar };
