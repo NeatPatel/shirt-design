e637b4c1782fe40695bd92655b4e4c6c873b1283
soak7 <micahcheng@gmail.com>
2024-08-05 20:00:00 -0700

    Created template for About and Pricing page
    +make template for about page
    +make some of the template for pricing page

--- App/src/pages/About.jsx
+++ App/src/pages/About.jsx
@@ -7,9 +7,52 @@ import TripleBlock from '../components/TripleBlock.jsx';
 import { Button, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import shirt from '../assets/shirt.png';
+
 function About(){
     return (<>
-        <h1>about</h1>
+        <ContentBlock content={<>
+            <h1 class="display-1">About Us</h1>
+        </>} mt="15" mb="15" />
+
+        <SplitBlock 
+            left={<>
+                <h1>Our Mission</h1>
+            </>}
+            
+            right={<>
+                <h4>Lorem ipsum odor amet, consectetuer adipiscing elit. Lobortis commodo accumsan ligula; molestie ac pretium nec. Ad euismod hac lectus eros consectetur venenatis. Phasellus massa taciti leo hendrerit donec accumsan. Phasellus mollis fames non nulla luctus iaculis. </h4>
+            </>}
+            
+            mt="0" mb="25" 
+        />
+
+        <SplitBlock 
+            left={<>
+                <h4>Aliquet accumsan magnis quam habitant dignissim vestibulum dolor vehicula. Dapibus orci sem natoque turpis malesuada per taciti luctus. Class velit efficitur eget class rhoncus. Integer morbi sociosqu diam egestas eros accumsan magna vitae.</h4>
+            </>}
+            
+            right={<>
+                <h1>Our Story</h1>
+            </>}
+            
+            mt="5" mb="25" 
+        />
+
+        <SplitBlock 
+            left={<>
+                <h1>Why Wait?</h1>
+                <h1>Style Your Dream Shirt Today!</h1>
+            </>}
+            
+            right={<>
+                <Image
+                    src={shirt}
+                />
+            </>}
+            
+            mt="5" mb="25" 
+        />
     </>)
 }
 
--- App/src/pages/Pricing.jsx
+++ App/src/pages/Pricing.jsx
@@ -9,7 +9,43 @@ import { Link } from 'react-router-dom';
 
 function Pricing(){
     return (<>
-        <h1>$$$$$$$$$$$$</h1>
+        <ContentBlock content={<>
+            <h1 class="display-1">Pricing</h1>
+        </>} mt="15" mb="15" />
+
+        <ContentBlock content={<>
+            <h1 class="text-primary">Individual</h1>
+        </>} mt="15" mb="15" />
+
+        <SplitBlock 
+            left={<>
+                <h1 class="text-right me-0">T-Shirts</h1>
+            </>}
+            
+            right={<>
+                <h1>Long-Sleeve Shirts</h1>
+            </>}
+            
+            mt="0" 
+            mb="25" 
+        />
+
+        <TripleBlock 
+            left={<>
+                <h1>Jackets</h1>
+            </>}
+
+            middle={<>
+                <h1>Sweaters</h1>
+            </>}
+            
+            right={<>
+                <h1>Hoodies</h1>
+            </>}
+            
+            mt="0" 
+            mb="25" 
+        />
     </>)
 }
 
